8eb79769375926c6c9cdb0c96e8ae27c3c074dec
Madalina <114758840+madalinadeni@users.noreply.github.com>
2025-05-09 20:38:42 +0300

    Add files via upload

---
+++ SecuritateRetele.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"crypto/x509"
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+)
+
+func verifyCertificate(cert *x509.Certificate, hostname string) error {
+	if time.Now().Before(cert.NotBefore) || time.Now().After(cert.NotAfter) {
+		return fmt.Errorf("Certificatul este invalid. Posibil un atac 'Man in the middle'!")
+	}
+
+	err := cert.VerifyHostname(hostname)
+	if err != nil {
+		return fmt.Errorf("Numele domeniului nu corespunde certificatului.")
+	}
+
+	return nil
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("Eroare la argumentele din linia de comanda!")
+		return
+	}
+	hostname := os.Args[1]
+	url := "https://" + hostname
+
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Printf("Eroare la request: %v\n", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	tlsConnState := resp.TLS
+	if tlsConnState == nil || len(tlsConnState.PeerCertificates) == 0 {
+		fmt.Println("Certificatul TLS nu a putut fi obtinut!")
+		return
+	}
+	cert := tlsConnState.PeerCertificates[0]
+
+	err = verifyCertificate(cert, hostname)
+	if err != nil {
+		fmt.Printf("Site-ul este nesigur. Posibil un atac 'Man in the middle'!:\n %v\n", err)
+	} else {
+		fmt.Println("Site-ul este sigur!")
+	}
+}
